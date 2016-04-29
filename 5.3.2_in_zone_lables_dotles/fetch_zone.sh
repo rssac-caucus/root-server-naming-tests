f28d7aa1a08d89ea6b97b3c0e951920b41858d61
b4ldr <github@johnbond.org>
2016-04-29 10:19:58 +0100

    update script

--- 5.3.2_in_zone_lables_dotles/fetch_zone.sh
+++ 5.3.2_in_zone_lables_dotles/fetch_zone.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-ZONE=$1
-SERVER=$2
-
-if [ "${ZONE}" == "." ]
-then
-  FILE=/var/lib/knot/root.zone
-else
-  FILE=/var/lib/knot/${ZONE}.zone
-fi
 
 mkdir -p /var/lib/knot/
-dig AXFR ${ZONE} @${SERVER} |  egrep -v "NSEC|RRSIG|DNSKEY|;" | sed s/.root-servers.net//g > ${FILE}
+dig AXFR . @xfr.dns.icann.org |  egrep -v "NSEC|RRSIG|DNSKEY|;" | sed s/.root-servers.net//g > /var/lib/knot/root.zone
