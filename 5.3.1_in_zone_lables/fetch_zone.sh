61a498bede0becbf5385eab82a03f0298e5601fa
b4ldr <github@johnbond.org>
2016-05-09 12:50:00 +0100

    updatre vagrant files

--- 5.3.1_in_zone_lables/fetch_zone.sh
+++ 5.3.1_in_zone_lables/fetch_zone.sh
@@ -2,4 +2,4 @@
 
 
 mkdir -p /var/lib/knot/
-dig AXFR . @xfr.dns.icann.org |  egrep -v "NSEC|RRSIG|DNSKEY|;" | sed s/root-servers.net/root-servers/g > /var/lib/knot/root.zone
+dig AXFR . @xfr.dns.icann.org |  egrep -v "NSEC|RRSIG|DNSKEY|;" | sed s/root-servers.net/root-serversasdasdasdasdasdasda/g > /var/lib/knot/root.zone
