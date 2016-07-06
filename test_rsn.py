d150b2d3e416d41a01c34366a6cda65cb68f6106
b4ldr <github@johnbond.org>
2016-07-06 12:25:05 +0100

    update

--- test_rsn.py
+++ test_rsn.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python
 
-import dns
 import argparse
 import logging
+from dns.flags import DO
+from dns.resolver import query, Resolver
+
+class RsnServer(object):
+    def __init__(self, server):
+        self.logger           = logging.getLogger('RsnServer')
+        self.server           = server
+        self.ipv4             = query(self.server, 'A')[0].address
+        self.ipv6             = query(self.server, 'AAAA')[0].address
+        self.resolver         = Resolver()
+
+        self.logger.debug('initiate: {} ({}/{})'.format(self.server, self.ipv4, self.ipv6))
+        self.update_sizes()
+
+    def _update_size(self, server, dnssec):
+        '''get the response size'''
+        self.resolver.nameservers = [ server ]
+        if dnssec:
+            self.resolver.use_edns(0,DO,4096)
+        else:
+            self.resolver.use_edns(0,0,4096)
+        answer = self.resolver.query('.', 'NS')
+        size   = len(answer.response.to_wire())
+        self.logger.debug('Size:{}:{}'.format(self.server, size))
+        return size
+
+    def update_sizes(self):
+        self.size_ipv4        = self._update_size(self.ipv4, False)
+        self.size_ipv6        = self._update_size(self.ipv6, False)
+        self.size_ipv4_dnssec = self._update_size(self.ipv4, True)
+        self.size_ipv6_dnssec = self._update_size(self.ipv6, True)
+
 
 def get_args():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -10,6 +41,7 @@ def get_args():
     parser.add_argument('-s', '--server', default='127.0.0.1' )
     parser.add_argument('-b', '--bufsize', type=int, default=4096 )
     parser.add_argument('-v', '--verbose', action='count' )
+    parser.add_argument('servers_file')
     return parser.parse_args()
 
 def set_log_level(args_level):
@@ -24,10 +56,13 @@ def set_log_level(args_level):
 
 
 def main():
-    nsset = [a.root-servers.org : { 'A', 'AAAA' 
     args = get_args()
     set_log_level(args.verbose)
-    get_nsset
+    servers = []
+    with open(args.servers_file) as f:
+        for line in f.read().splitlines():
+            logging.debug('loading {}'.format(line))
+            servers.append(RsnServer(line))
 
 if __name__ == '__main__':
     main()
